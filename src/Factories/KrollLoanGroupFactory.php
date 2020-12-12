3be562e40d4e6ed79070652148daad7013693f93
Michael Drennen <michaeldrennen74@gmail.com>
2020-12-12 08:53:04 -0700

    Dealing with an empty UUID.

--- src/Factories/KrollLoanGroupFactory.php
+++ src/Factories/KrollLoanGroupFactory.php
@@ -23,10 +23,10 @@ class KrollLoanGroupFactory {
         $objectVars[ 'pari_passu_details' ] = json_encode( $objectVars[ 'pari_passu_details' ] );
 
         // Remove these values from the array and put them in their own fields.
-        $objectVars[ 'pari_deal_in_control_uuid' ] = $objectVars[ 'pari_deal_in_control' ][ 'uuid' ];
+        $objectVars[ 'pari_deal_in_control_uuid' ] = empty( $objectVars[ 'pari_deal_in_control' ][ 'uuid' ] ) ? '' : $objectVars[ 'pari_deal_in_control' ][ 'uuid' ];
         $objectVars[ 'pari_deal_in_control_name' ] = empty( $objectVars[ 'pari_deal_in_control' ][ 'name' ] ) ? '' : $objectVars[ 'pari_deal_in_control' ][ 'name' ];
         unset( $objectVars[ 'pari_deal_in_control' ] );
-        $objectVars[ 'pari_kbra_master_deal_uuid' ] = $objectVars[ 'pari_kbra_master_deal' ][ 'uuid' ];
+        $objectVars[ 'pari_kbra_master_deal_uuid' ] = empty( $objectVars[ 'pari_kbra_master_deal' ][ 'uuid' ] ) ? '' : $objectVars[ 'pari_kbra_master_deal' ][ 'uuid' ];
         $objectVars[ 'pari_kbra_master_deal_name' ] = empty( $objectVars[ 'pari_kbra_master_deal' ][ 'name' ] ) ? '' : $objectVars[ 'pari_kbra_master_deal' ][ 'name' ];
         unset( $objectVars[ 'pari_kbra_master_deal' ] );
 
