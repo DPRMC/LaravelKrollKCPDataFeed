a7c1c186797faa12c6d762e9040929d690e60ad9
Michael Drennen <michaeldrennen74@gmail.com>
2020-12-21 16:20:37 -0700

    Adding repositories.

--- src/Repositories/KrollDealRepository.php
+++ src/Repositories/KrollDealRepository.php
@@ -9,10 +9,11 @@ use DPRMC\LaravelKrollKCPDataFeed\Models\KrollDeal;
 class KrollDealRepository {
 
 
-    public function getRecent(int $daysAgo=7){
-        $earliestDate = Carbon::now(Helper::CARBON_TIMEZONE)->subDays($daysAgo);
-        return KrollDeal::where(KrollDeal::generated_date, '<', $earliestDate)
-                        ->orderBy(KrollDeal::generated_date)
+    public function getRecent( int $daysAgo = 7 ) {
+        $earliestDate = Carbon::now( Helper::CARBON_TIMEZONE )->subDays( $daysAgo );
+        return KrollDeal::with( [ KrollDeal::bonds, KrollDeal::loanGroups, KrollDeal::paidOffLiquidatedLoanGroups ] )
+                        ->where( KrollDeal::generated_date, '<', $earliestDate )
+                        ->orderBy( KrollDeal::generated_date )
                         ->get();
     }
 
