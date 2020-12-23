2fd6a060bd9a3b16b4edadaa16ddd2b0b55dbff4
Michael Drennen <michaeldrennen74@gmail.com>
2020-12-23 13:16:38 -0700

    Update.

--- src/Repositories/KrollDealRepository.php
+++ src/Repositories/KrollDealRepository.php
@@ -9,11 +9,34 @@ use DPRMC\LaravelKrollKCPDataFeed\Models\KrollDeal;
 class KrollDealRepository {
 
 
+    /**
+     *
+     */
+    const RELATIONSHIPS = [ KrollDeal::bonds,
+                            KrollDeal::loanGroups,
+                            KrollDeal::paidOffLiquidatedLoanGroups ];
+
+
+    /**
+     * @param int $daysAgo
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function getRecent( int $daysAgo = 7 ) {
         $earliestDate = Carbon::now( Helper::CARBON_TIMEZONE )->subDays( $daysAgo );
-        return KrollDeal::with( [ KrollDeal::bonds, KrollDeal::loanGroups, KrollDeal::paidOffLiquidatedLoanGroups ] )
+        return KrollDeal::with( self::RELATIONSHIPS )
                         ->where( KrollDeal::generated_date, '<', $earliestDate )
                         ->orderBy( KrollDeal::generated_date )
+                        ->get();
+    }
+
+
+    /**
+     * @param string $uuid
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getByUuid( string $uuid ) {
+        return KrollDeal::with( self::RELATIONSHIPS )
+                        ->where( KrollDeal::uuid, $uuid )
                         ->get();
     }
 
